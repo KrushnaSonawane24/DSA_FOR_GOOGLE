f5e0ee2b5e26561403de1e8e164559231f568aec
Krushna <sonawanekrushna830@gmail.com>
2026-01-13 07:21:01 +0530

    Add day_2.cpp

---
+++ day_2.cpp
@@ -0,0 +1,26 @@
+//14. Longest Common Prefix
+
+include <iostream>
+using namespace std;
+int main(){
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0) return "";
+
+        string prefix = strs[0];
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char currentChar = prefix[i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].length() || strs[j][i] != currentChar) {
+                    return prefix.substr(0, i);
+                }
+            }
+        }
+
+        return prefix;
+    }
+};
+}
