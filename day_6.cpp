4f239965ce5f676b417a39758ebe11b61d03070e
Krushna <sonawanekrushna830@gmail.com>
2026-01-17 08:07:44 +0530

    day_6 of DSA

---
+++ day_6.cpp
@@ -0,0 +1,19 @@
+// leetcode day_6 problem stetment no 168. Excel Sheet Column Title
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result ="";
+
+        while(columnNumber > 0){
+            columnNumber--;
+            int rem = columnNumber%26;
+            char ch='A'+ rem;     
+            result= ch + result;
+            columnNumber/=26;
+
+
+        }
+        return result;
+    }
+};
