9194960ff9a624e483be9ea47a8f9e77597c1ddf
Krushna <sonawanekrushna830@gmail.com>
2026-01-28 08:44:08 +0530

    day 17 of dsa

---
+++ day_16.cpp
@@ -0,0 +1,16 @@
+//day 17 problem no 409. Longest Palindrome
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int expected = n*(n+1)/2;
+        int actual = 0;
+
+        for (int x:nums){
+            actual += x;
+        }
+        return expected - actual;
+        
+    }
+};
