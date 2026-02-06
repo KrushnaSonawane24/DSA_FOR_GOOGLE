7e85886eda64a429c2e7212cae63ea7e371e4d02
Krushna <sonawanekrushna830@gmail.com>
2026-02-06 07:47:14 +0530

    day 19 of dsa

---
+++ day_19.cpp
@@ -0,0 +1,20 @@
+//leetcode day_6 problem stetment no 66. Plus One 
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        for (int i=n -1;i>=0;i--){
+            if(digits[i]<9) {
+                digits[i] ++;
+                return digits;
+            }
+
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+        
+    }
+};
